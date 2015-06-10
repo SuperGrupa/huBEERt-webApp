bbb4ed47eadcbedc9000682fafdc85c8f3a3932d
Daniel Koza <dako@MacBook-Pro-Daniel.local>
2015-06-10 10:42:26 +0200

    config e2e tests

--- test/e2e/main.scenario.coffee
+++ test/e2e/main.scenario.coffee
@@ -1,11 +1,7 @@
 'use strict'
 
-utils = require('../utils')
-
 describe 'huBEERt', ->
-  ptor = {}
   beforeEach ->
-    ptor = protractor.getInstance()
     console.info('\nrunning:', jasmine.getEnv().currentSpec.description)
 
   afterEach ->
@@ -13,5 +9,5 @@ describe 'huBEERt', ->
       utils.takeScreenshot(browser, jasmine.getEnv().currentSpec.description.replace(/\ /g, '-'))
 
   it 'should load the main page', ->
-    ptor.get('/')
-    expect(ptor.isElementPresent(By.css('body'))).toBe(true)
+    browser.get('/')
+    expect(browser.isElementPresent(By.css('body'))).toBe(true)
--- test/e2e/protractor.config.js
+++ test/e2e/protractor.config.js
@@ -16,18 +16,13 @@ exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
 
   multiCapabilities: [
-  {
-    'browserName': 'firefox'
-  },
-  {
-    'browserName': 'phantomjs',
-    'phantomjs.binary.path': require('phantomjs').path
-  }
-  ],
-  onPrepare: function() {
-    require('jasmine-reporters');
-    jasmine.getEnv().addReporter(
-      new jasmine.JUnitXmlReporter('shippable/testresults', true, true, 'junit')
-    );
-  }
+    {
+      'browserName': 'chrome'
+    }
+  //},
+  //{
+  //  'browserName': 'phantomjs',
+  //  'phantomjs.binary.path': require('phantomjs').path
+  //}
+  ]
 };
